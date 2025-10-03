ce7702415cc5d20d15b68ee6f387e2c4e34b2b64
Alex <xrlogic1@gmail.com>
2025-10-03 16:17:10 +0400

    Multicast

---
+++ Milticast/Program.cs
@@ -0,0 +1,38 @@
+﻿// Unicast -> server -> one client || one client -> one client
+// Multicast -> server -> many clients || one client -> many clients'
+// Broadcast -> server -> all clients
+//
+//
+//
+// Subnet Mask = 255.255.255.0
+//
+//
+// 172.20.208.1 - 172.20.208.254
+//
+// 172.20.208.255 - broadcast address
+
+
+// 224.0.0.0 to 239.255.255.255 (multicast)
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var multicastAddress = IPEndPoint.Parse("231.123.12.7:7788");
+using var updClient = new UdpClient();
+
+while (true)
+{
+    var message = Console.ReadLine()!;
+
+    if (message == "exit")
+    {
+        break;
+    }
+    updClient.Send(Encoding.UTF8.GetBytes(message), multicastAddress);
+}
+
+
+
+    
+    
---
+++ MulticastReceiver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var multicastAddress = IPEndPoint.Parse("231.123.12.7");
+var udpClient = new UdpClient(7788);
+
+udpClient.JoinMulticastGroup(multicastAddress.Address);
+
+while (true)
+{
+    var result = await udpClient.ReceiveAsync();
+
+    Console.WriteLine(Encoding.UTF8.GetString(result.Buffer));
+}
